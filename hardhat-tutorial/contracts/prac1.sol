b36ca04a037afde4da6abfb5571319d42a43f7a0
Rashford <mikearowo@gmail.com>
2023-06-21 22:49:55 +0100

    new

---
+++ hardhat-tutorial/contracts/prac1.sol
@@ -0,0 +1,20 @@
+// Define the compiler version you would be using
+
+pragma solidity ^0.8.19;
+
+// start by creating a contract named Variables
+contract Variables {
+    /*
+        ******** State variables **********
+    */
+    /*
+    uint stands for unsigned integer, meaning non negative integers
+    different sizes are available. Eg
+        - uint8   ranges from 0 to 2 ** 8 - 1
+        - uint256 ranges from 0 to 2 ** 256 - 1
+    `public` means that the variable can be accessed internally
+     by the contract and can also be read by the external world
+    */
+   uint8 public u8 = 10;
+   
+}
