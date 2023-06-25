bf2c630558b763b2a784d6e2275137a65cf56813
Rashford <mikearowo@gmail.com>
2023-06-25 09:34:58 +0100

    new

--- hardhat-tutorial/PRAC/pracL.sol
+++ hardhat-tutorial/PRAC/pracL.sol
@@ -1,3 +1,19 @@
 // LIBRARIES
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.10:
+
+library SafeMath{
+   function add(uint x, uint y) internal pure returns (uint) {
+    uint z = x + y;
+    
+    // if z is overflowed, throw an error
+    require(z >= x, "uint overflow");
+    return z;
+   } 
+}
+
+contract TestSafeMath {
+    function testAdd(uint x, uint y) public pure returns (uint) {
+        return SafeMath.add(x, y);
+    }
+}
